61c69e1fcec05c2a382782b763e8877094b35f5d
Sonic <sf7293gv@go.minnstate.edu>
2022-03-02 14:40:19 -0600

    New additions to be uploaded

--- app/src/main/java/com/example/landscapelayout/MainActivity.kt
+++ app/src/main/java/com/example/landscapelayout/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 
 const val EXTRA_SQUARE_SIZE = "com.example.landscapelayout.tap_the_square.SQUARE.SIZE"
 
@@ -14,6 +17,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var seekBar: SeekBar
     private lateinit var seekBarLabel: TextView
     private lateinit var showSquareButton: Button
+
+    private val squareResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result -> handleSquareResult(result)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,7 +57,25 @@ class MainActivity : AppCompatActivity() {
         // launch square activity, set size of square based on the progress
         val showSquareIntent = Intent(this, SquareActivity::class.java)
         showSquareIntent.putExtra(EXTRA_SQUARE_SIZE, seekBar.progress)
-        startActivity(showSquareIntent)
+//        startActivity(showSquareIntent)
+        squareResultLauncher.launch(showSquareIntent)
+    }
+
+    private fun handleSquareResult(result: ActivityResult) {
+        // Display result to user
+        if (result.resultCode == RESULT_OK) {
+            val intent = result.data
+            val tapped = intent?.getBooleanExtra(EXTRA_TAPPED_INSIDE_SQUARE, false) ?: false
+            val message = if (tapped) {
+                getString(R.string.tapped_square)
+            } else {
+                getString(R.string.missed_square)
+            }
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        } else if (result.resultCode == RESULT_CANCELED) {
+            Toast.makeText(this, getString(R.string.did_not_try), Toast.LENGTH_SHORT).show()
+
+        }
     }
 
 }
